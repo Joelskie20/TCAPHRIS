210014b619ae52109f727950231b45eeea9ec524
Kiel Legaspi Jose <kiel.legaspi.jose@gmail.com>
2018-12-20 10:26:02 +0800

    'Carbon' class can now be access globally, add UI for Time-In/Time-Out

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,17 +7,39 @@
       <!-- Content Header (Page header) -->
       <section class="content-header">
         <h1>
-          Page Header
-          <small>Optional description</small>
+          Dashboard
         </h1>
       </section>
 
       <!-- Main content -->
       <section class="content container-fluid">
 
-        <!--------------------------
-          | Your Page Content Here |
-          -------------------------->
+        <div class="col-xs-12 col-sm-6">
+					
+							<!-- TIME CLOCK -->
+							<div class="box">
+								<div class="box-header with-border">
+									<h3 class="box-title">Time Clock</h3>
+								</div>
+								<div class="box-body">
+									<div class="row">
+										<div class="col-xs-12 col-sm-6">
+											<button class="btn btn-primary btn-block btn-lg">TIME IN</button>
+										</div>
+										<div class="col-xs-12 col-sm-6">
+											<button class="btn btn-default btn-block btn-lg disabled">TIME OUT</button>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-xs-12">
+											<h1 class="pull-left">{{ Carbon::now()->format('F d, Y') }}</h1>
+											<h1 class="pull-right">{{ Carbon::now()->format('g:i A') }}</h1>
+										</div>
+									</div>
+								</div>
+							</div>
+							
+						</div>
 
       </section>
       <!-- /.content -->
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -20,9 +20,9 @@
     <ul class="sidebar-menu" data-widget="tree">
     <li class="header">MAIN NAVIGATION</li>
     <!-- Optionally, you can add icons to the links -->
-    <li class="active"><a href="#"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-    <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
-    <li class="treeview">
+    <li class="{{ route('dashboard') ? 'active' : '' }}"><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+    {{-- <li><a href="#"><i class="fa fa-calendar"></i> <span>Daily Time Records</span></a></li> --}}
+    {{-- <li class="treeview">
         <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
         <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
@@ -32,7 +32,7 @@
         <li><a href="#">Link in level 2</a></li>
         <li><a href="#">Link in level 2</a></li>
         </ul>
-    </li>
+    </li> --}}
     </ul>
     <!-- /.sidebar-menu -->
 </section>
